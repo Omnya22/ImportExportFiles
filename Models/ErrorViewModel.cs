a6ee8c875f8de023723f0101f18c6b7a03548504
Omnya Elhosseny <omnya@jawda.net>
2024-10-02 13:36:31 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImportExportFiles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
