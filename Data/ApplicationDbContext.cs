f9eadc647fab467e332692e51dbd774036136354
Omnya Elhosseny <omnya@jawda.net>
2024-10-02 14:16:34 +0300

    add product entity & init migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ImportExportFiles.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImportExportFiles.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .HasIndex(p => p.PartSku)
+            .IsUnique();
+    }
+}
---
+++ Data/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImportExportFiles.Data.Entities;
+public class Product
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public string BandNumber { get; set; }
+    [Required]
+    public string CategoryCode { get; set; }
+    [Required]
+    public string Manufacturer { get; set; }
+    [Required]
+    public string PartSku { get; set; }
+    [Required]
+    public string ItemDescription { get; set; }
+    [Required]
+    public decimal ListPrice { get; set; }
+    [Required, Range(0, 100)]
+    public decimal MinDiscount { get; set; }
+    [Required]
+    public decimal DiscountPrice { get; set; }
+}
