ee6b41aadb90588520d75de489032c19fd831f4e
Omnya Elhosseny <omnya@jawda.net>
2024-10-02 15:15:09 +0300

    add uow

---
+++ UOW/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using ImportExportFiles.Data;
+using ImportExportFiles.Interfaces;
+using ImportExportFiles.Repositories;
+
+namespace ImportExportFiles.UOW;
+
+public class UnitOfWork(ApplicationDbContext context, IProductRepository productRepository) : IUnitOfWork
+{
+    public IProductRepository Products => productRepository ??= new ProductRepository(context);
+    public async Task<int> CompleteAsync() => await context.SaveChangesAsync();
+    public void Dispose() => context.Dispose();
+}
+
