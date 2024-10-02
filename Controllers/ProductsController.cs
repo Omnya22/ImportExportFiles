26b230dbccc35a082ca21093914659779354139d
Omnya Elhosseny <omnya@jawda.net>
2024-10-02 14:27:28 +0300

    add product controller & related views

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImportExportFiles.Controllers;
+
+public class ProductsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Import()
+    {
+        return View();
+    }
+}
---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImportExportFiles.Models;
+
+public class ProductViewModel
+{
+    [Required]
+    public string BandNumber { get; set; }
+    [Required] 
+    public string CategoryCode { get; set; }
+    [Required] 
+    public string Manufacturer { get; set; }
+    [Required] 
+    public string PartSku { get; set; }
+    [Required] 
+    public string ItemDescription { get; set; }
+    [Required] 
+    public decimal ListPrice { get; set; }
+    [Required, Range(0,100)] 
+    public decimal MinDiscount { get; set; }
+    [Required] 
+    public decimal DiscountPrice { get; set; }
+}
